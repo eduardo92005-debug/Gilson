bb9ef64cdb88754786a98c544f7c211397cf072d
Carlos Eduardo <78118717+eduardo92005-debug@users.noreply.github.com>
2021-07-30 06:35:18 -0300

    Update Lista_05.py

--- Exercício-05/Lista_05.py
+++ Exercício-05/Lista_05.py
@@ -13,6 +13,7 @@
       outra plataforma). Para formar figuras o número de iterações deve ser por volta de 1000
 '''
 ##### Bibliotecas #####
+
 import random
 import matplotlib.pyplot as plt
 import MeuModulo as MeMo # MeMo.funcãoBlabla é como você deve chamar as funções
@@ -21,23 +22,20 @@ random.seed(0) # não modifique esta linha para que o exercício dê as mesmas r
 
 # Leitura dos valores de entrada:
 n=int(input('iterações: '))
-print('Probabilidades: ',end='')
+print('Probabilidades: ', end='')
 p=MeMo.LeListaFloat()
-print('cx: ',end='')
+print('cx: ', end='')
 cx=MeMo.LeMatriz()
-print('cy: ',end='')
+print('cy: ', end='')
 cy=MeMo.LeMatriz()
 
 # escreva aqui a chamada da função ifs():
-
-# impressão das listas:
-MeMo.ImprimeListaFloat(X)
-MeMo.ImprimeListaFloat(Y)
-
+points_xy = MeMo.IFS(n, p, cx, cy)
 # Plot do gráfico:
-#plt.scatter(X,Y,marker='.',c='deeppink',alpha=0.5)
-#plt.grid()
-#plt.xlabel("Eixo X")
-#plt.ylabel("Eixo Y")
-#plt.show()
+print(points_xy)
+plt.scatter(points_xy[0], points_xy[1], marker='.', c='deeppink', alpha=0.5)
+plt.grid()
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
+plt.show()
 # Veja documentação em https://matplotlib.org/stable/api/pyplot_summary.html
