bb68c2a846705edbce66e5e8dd24e6c2c73406bf
Carlos Eduardo <78118717+eduardo92005-debug@users.noreply.github.com>
2021-08-30 19:22:07 -0300

    Create Stack.py

---
+++ Stack.py
@@ -0,0 +1,27 @@
+class ErrorHandling:
+    pass
+class Empty(ErrorHandling, Exception):
+    pass
+
+class Stack:
+    def __init__(self):
+        self.__stack = []
+    def push(self, item):
+        " Insert a element to final position"
+        self.__stack += [item]
+    def pop(self):
+        " Remove the top element"
+        return self.__stack.pop()  
+    def __len__(self):
+        "Returns the size the stack"
+        return len(self.__stack)
+    def is_empty(self):
+        "Check if stack is empty"
+        return len(self.__stack) == 0
+    def peek(self):
+        " Returns the top element of the stack"
+        if self.is_empty():
+            raise Empty("Stack is Empty!")
+        return self.__stack[-1]
+    
+        
